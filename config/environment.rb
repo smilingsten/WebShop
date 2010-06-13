8a60e59f970804e8098f6d0e02e3a520190a28dd
Sten <smilingsten@gmail.com>
2010-06-13 17:57:45 +0200

    shopping cart added, but it has to be saved in database, currently object saved in cookie

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
+
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
@@ -38,4 +39,5 @@ Rails::Initializer.run do |config|
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
+
 end
