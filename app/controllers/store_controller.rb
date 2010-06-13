8a75e4f0ca1c008ed3028dc0a60cb882f1828d08
Sten <smilingsten@gmail.com>
2010-06-13 18:13:14 +0200

    not able to add wrong products to shopping cart manually

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,11 +2,16 @@ class StoreController < ApplicationController
   def index
   	@products = Product.find_products_for_sale
   end
-    def add_to_cart 
-	product = Product.find(params[:id]) 
-		@cart = find_cart 
-		@cart.add_product(product) 
- end
+ def add_to_cart 
+product = Product.find(params[:id]) 
+@cart = find_cart 
+@cart.add_product(product) 
+rescue ActiveRecord::RecordNotFound 
+logger.error("Attempt to access invalid product #{params[:id]}") 
+flash[:notice] = "Invalid product" 
+redirect_to :action => 'index' 
+end 
+
   
   private 
   def find_cart 
